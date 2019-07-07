288ad6fec462fa4611743f9494e48654c1c1ba20
Ivan <Kishkan91@gmail.com>
2019-07-08 01:51:39 +0400

    DoubleFormatter minor refactor

--- src/com/kishkan91/services/DoubleFormatter.java
+++ src/com/kishkan91/services/DoubleFormatter.java
@@ -1,9 +1,7 @@
 package com.kishkan91.services;
 
 public class DoubleFormatter {
-
     public static double formatDouble(double result) {
-
         String formattedResult = String.format("%.2f", result);
         formattedResult = formattedResult.replaceAll(",", ".");
         return (Double.valueOf(formattedResult));
